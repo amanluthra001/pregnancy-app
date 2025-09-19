26c5a58dfb2f01b59ad32ce63ad829d0973db8fe
AMAN LUTHRA <amanluthra804@gmail.com>
2025-09-19 22:46:11 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,16 @@ import doctorRoutes from "./routes/doctor.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
